5542d66b96724470042592e0ff30f5d967d7a263
Spencer Lefever <slefever1@gulls.salisbury.edu>
2022-10-18 12:01:16 -0400

    Uploading task1.2

--- COSC350Lab4/task1.2.c
+++ COSC350Lab4/task1.2.c
@@ -25,32 +25,51 @@ int main(int argc, char** argv) {
     other = creat("other", PERMISSIONS);
 
     while(read(input, buf, 1) > 0) {
-        //Case if buf is a number
-        if(*buf >= 48 && *buf <= 57){
-            //Write the buf into num
+        if(*buf == '\n') {
             write(num, buf, 1);
-            //Write blank space into other files
-            write(alpha, " ", 1);
-            write(other, " ", 1);
-        }
-        //Else if buf is a character
-        else if((*buf >= 65 && *buf <= 90) || (*buf >= 97 && *buf <= 122)) {
-            //Write character into alpha file
             write(alpha, buf, 1);
-            //Write blank spaces into other files
-            write(num, " ", 1);
-            write(other, " ", 1);
-
-        }
-        //Buf is neither number nor character
-        else {
-            //Write special character to other
             write(other, buf, 1);
-            //Write blank spaces to the other files
-            write(alpha, " ", 1);
-            write(num, " ", 1);
+        }
+        else{
+            //Case if buf is a number
+            if(*buf >= 48 && *buf <= 57){
+                //Write the buf into num
+                write(num, buf, 1);
+                //Write blank space into other files
+                write(alpha, " ", 1);
+                write(other, " ", 1);
+            }
+            //Else if buf is a character
+            else if((*buf >= 65 && *buf <= 90) || (*buf >= 97 && *buf <= 122)) {
+                //Write character into alpha file
+                write(alpha, buf, 1);
+                //Write blank spaces into other files
+                write(num, " ", 1);
+                write(other, " ", 1);
+
+            }
+            //Buf is neither number nor character
+            else if (*buf != '\n'){
+                //Write special character to other
+                write(other, buf, 1);
+                //Write blank spaces to the other files
+                write(alpha, " ", 1);
+                write(num, " ", 1);
+            }
+            else {
+                write(alpha, " ", 1);
+                write(num, " ", 1);
+                write(other, " ", 1);
+            }
         }
     }
+    close(alpha);
+    close(num);
+    close(other);
+
+    alpha = open("alpha", PERMISSIONS);
+    num = open("num", PERMISSIONS);
+    other = open("other", PERMISSIONS);
 
     copy = creat("copy", PERMISSIONS);
 
@@ -59,17 +78,20 @@ int main(int argc, char** argv) {
     char* numBuf = malloc(1);
     char* alphaBuf = malloc(1);
     char* otherBuf = malloc(1);
+    
+
     //Make copy of the file from alpha, num and others
     //All files are same size so we can loop by reading from input
 
     //lseek to move file offsets to front
-    int offset = lseek(input, 0, SEEK_SET);
+    int fsize = lseek(input, 0, SEEK_END);
     
-    lseek(alpha, 0, SEEK_SET);
-    lseek(num, 0, SEEK_SET);
-    lseek(other, 0, SEEK_SET);
-    
-    while(read(input, buf, 1) > 0) {
+    fsync(alpha);
+    fsync(num);
+    fsync(other);
+
+
+    for(int i=0; i<fsize; i++) {
         read(alpha, alphaBuf, 1);
         read(num, numBuf, 1);
         read(other, otherBuf, 1);
@@ -78,19 +100,22 @@ int main(int argc, char** argv) {
         if(*alphaBuf == asciiSpace && *numBuf == asciiSpace && *otherBuf == asciiSpace) {
             write(copy, otherBuf, 1);
         }
-        //If alpha is not whitespace print
-        if(*alphaBuf != asciiSpace) {
-            write(copy, alphaBuf, 1);
+        else if(*alphaBuf == '\n' && *numBuf == '\n' && *otherBuf == '\n') {
+            write(copy, "\n", 1);
         }
-        //If num is not whitespace print
-        else if(*numBuf != asciiSpace) {
-            write(copy, numBuf, 1);
-        }
-        //If other is not whitespace print
         else {
-            write(copy, otherBuf, 1);
+            //If alpha is not whitespace print
+            if(*alphaBuf != asciiSpace) {
+                write(copy, alphaBuf, 1);
+            }
+            //If num is not whitespace print
+            if(*numBuf != asciiSpace) {
+                write(copy, numBuf, 1);
+            }
+            //If other is not whitespace print
+            if (*otherBuf != asciiSpace) {
+                write(copy, otherBuf, 1);
+            }
         }
-
     }
-
 }
