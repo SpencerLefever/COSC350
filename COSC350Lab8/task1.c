a01fdf831fe166291ad557f398034dff1e090efc
Spencer Evan Lefever <slefever1@gulls.salisbury.edu>
2022-11-14 10:27:23 -0500

    First tasks of lab 8

---
+++ COSC350Lab8/task1.c
@@ -0,0 +1,55 @@
+/**
+ * Spencer Lefever
+ * COSC350 Lab8 Task1
+ * 
+ * Concurrent Calculation
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#define NUM_THREADS 2
+
+int main(int argc, char** argv) {
+    if(argc != 2 ){
+    printf("ARGUMENT ERROR \n");
+    exit(-1);
+    }
+
+  input = atoi(argv[1]);
+  pthread_t threads[NUM_THREADS];
+
+  int rc = pthread_create(&threads[0],NULL,sum,(void *)input);
+  pthread_join(threads[0],NULL);
+
+  rc = pthread_create(&threads[1],NULL,factorial,(void *)input);
+  pthread_join(threads[0],NULL); 
+
+  if(rc != 0){
+    printf("ERROR\n");
+  }
+}
+
+void *sum(void *n){
+  int num = input;
+
+  for(int i=1;i<=num;i++){
+    sumNum += i;
+  }
+
+  printf("SUM: %d\n",sumNum);
+  pthread_exit(NULL);
+}
+
+void *factorial(){
+  int num = input;
+  int fact=1;
+  int i=1;
+
+  for(i;i<=num;i++){
+    fact = fact*i;
+  }
+
+  printf("FACTORIAL: %d\n",fact);
+  pthread_exit(NULL);
+}
---
+++ COSC350Lab8/task2.c
@@ -0,0 +1,70 @@
+/**
+ * Spencer Lefever
+ * COSC350 Lab8 Task2
+ * 
+ * Wait for a thread for concurrent calculations
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#define NUM_THREADS 4
+
+int scoreArray[20];
+
+
+int main(){
+    pthread_t t[NUM_THREADS];
+
+    pthread_create(&t[0],NULL,scores,NULL);
+    pthread_join(t[0],NULL);
+
+    sort();
+    
+    pthread_create(&t[1],NULL,avg,NULL);
+    pthread_create(&t[2],NULL,minMax,NULL);
+    pthread_join(t[2],NULL);
+
+}
+
+void sort()
+{
+    for (int i = 0; i < 19; i++)
+    {
+
+        int min = i;
+        for (int j = i+1; j < 20; j++){
+          if (scoreArray[j] < scoreArray[min]){            
+            min = j;
+          }
+        }
+
+        int temp = scoreArray[min];
+        scoreArray[min] = scoreArray[i];
+        scoreArray[i] = temp;
+    }
+}
+
+
+void *minMax(void *num){
+  printf("Min: %d\n",scoreArray[0]);
+  printf("Max: %d\n",scoreArray[19]);
+}
+void *scores(void *num){
+  for(int i=0;i<20;i++){
+    int num = 0;
+    scanf("%d",&num);
+    scoreArray[i] = num;
+  }
+}
+
+void *avg(void *num){
+  int sum = 0;
+  for(int i=0;i<20;i++){
+    sum += scoreArray[i];
+  }
+  double average = (double)sum/20;
+  int median = scoreArray[9];
+  printf("average: %f\n",average);
+  printf("median: %d\n",median);
+}
