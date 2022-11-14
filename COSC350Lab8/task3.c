ea6291ed2c5b1d7991f1728a018d3509f9fa8ff4
Spencer Evan Lefever <slefever1@gulls.salisbury.edu>
2022-11-14 12:48:54 -0500

    fixed task3

--- COSC350Lab8/task3.c
+++ COSC350Lab8/task3.c
@@ -25,25 +25,22 @@ int main(int argc, char *argv[]){
     char *num1 = argv[1];
     char *num2 = argv[2];
 
-    char buf[strlen(num1) + strlen(num2) + 1];
+    char* buf = malloc(strlen(num1) + strlen(num2) + 1);
     strncat(buf,num1,strlen(num1));
     strncat(buf, " ", 1);
     strncat(buf,num2,strlen(num2));
-
-    printf("OG buf %s sizes: %ld %ld\n", buf, strlen(num1), strlen(num2));
   
     int pid = fork();
     if(pid == 0){
         char buf2[128];
         close(mypipe[1]);
         read(mypipe[0],buf2,(strlen(num1)+strlen(num2)+1));
-        printf("Buf2 %s\n", buf2);
         char* c1 = buf2;
         int num12 = charToInt(c1);
-        char* c2 = buf2+strlen(num1);
+        char* c2 = buf2+strlen(num1)+1;
         int num22 = charToInt(c2);
 
-        printf("%s %s\n", c1, c2);
+        printf("C1: %s C2: %s\n", c1, c2);
         
         if((*c1 < 48 || *c1 > 57) && (*c2 < 48 || *c2 > 57)){
             printf("NOT A NUMBER\n");
@@ -64,7 +61,7 @@ int charToInt(char* c){
     int num = 0;
     for(int i=0; i<strlen(c); i++) {
         if(c[i] < 48 || c[i] > 57) {
-            return -1;
+            break;
         }
         num = num*10 + (c[i]-48);
     }
